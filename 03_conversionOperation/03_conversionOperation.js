13ca5ab9542ff6a15cfd23aaf3da9d58961e80df
RavigiriG <104981318+RavigiriG@users.noreply.github.com>
2023-11-26 19:09:03 +0000

    Operations

--- 03_conversionOperation/03_conversionOperation.js
+++ 03_conversionOperation/03_conversionOperation.js
@@ -23,6 +23,33 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *******************Operations*************************************************
+
+let value = 2
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); plus
+// console.log(2-2); minus
+// console.log(2*2); into
+// console.log(2**3); power
+// console.log(2/3); divide
+// console.log(2%3); Remender
+
+let str1 = "hello"
+let str2 = " Ravi"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3 + 4) * 5 % 3);
+
 
