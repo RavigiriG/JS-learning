56479cd1aaea2481d6a7748f497bdc1c7129b492
RavigiriG <104981318+RavigiriG@users.noreply.github.com>
2023-11-28 02:45:58 +0000

    comparision

---
+++ 04_comparision/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log(2 > 1);
+// console.log(2 >+1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log( 2!= 1); // Basic conversion
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // inpredectible / avoid this kind of code
+
+console.log(undefined == 0); // false in all comparisions/ avoid this kind of code
