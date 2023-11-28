f804359e9ebcd6f62a9b69222a989996d1dd3b40
RavigiriG <104981318+RavigiriG@users.noreply.github.com>
2023-11-28 03:16:28 +0000

    Datatype Summary

---
+++ 05_datatypes-summary/05_datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primative
+
+// 7 Types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol ('123')
+const anotherId = Symbol ('123')
+
+console.log(id  == anotherId);
+
+const bigNumber = 34234325455n
+
+
+
+// Reference (Non primative)
+
+// Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "ravi",
+    age: 25,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof outsideTemp); // null = object
