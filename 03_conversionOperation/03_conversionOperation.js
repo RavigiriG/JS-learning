07c97ee559db1bdf476a37c06439cee7af519554
RavigiriG <104981318+RavigiriG@users.noreply.github.com>
2023-11-28 02:34:20 +0000

    Operations

--- 03_conversionOperation/03_conversionOperation.js
+++ 03_conversionOperation/03_conversionOperation.js
@@ -44,12 +44,36 @@ let str2 = " Ravi"
 
 let str3 = str1 + str2
 
-console.log(str3);
+// console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
-console.log((3 + 4) * 5 % 3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+// let num 1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2 // don't do this, Not readable
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+// *******************Increment(++) Suffix & Preffix******************************************8
+let x = 5;
+const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+
+
+let a = 6;
+const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+
 
 
