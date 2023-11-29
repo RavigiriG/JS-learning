8e94e5899b42e5c970eadb130edaccff10e5b315
RavigiriG <104981318+RavigiriG@users.noreply.github.com>
2023-11-29 20:26:34 +0000

    Arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,10 @@
+const marvel_heroes = ["thor", "ironman", "spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+marvel_heroes.push(dc_heroes)
+
+console.log(marvel_heroes);
+console.log(marvel_heroes[3][2]);
+
+marvel_heroes.concat(dc_heroes)
+
