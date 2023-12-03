c1b33aea689e967365949644b94cef41c89ca181
RavigiriG <104981318+RavigiriG@users.noreply.github.com>
2023-12-03 08:45:44 +0000

    Objects Part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,6 +1,54 @@
 // const tinderUser = new Object() // singleton object
 const tinderUser = {}
 
-const tinderUser.id = ""
+tinderUser.id = "123abc"
+tinderUser.name = "123abc"
+tinderUser.isLoggedIn = false
 
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: "ravi",
+        lastname: "gausvami"
+    }
+}
+
+
+// console.log(regularUser.fullname.lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = { obj1, obj2 }
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "rk@gmail.com"
+    },
+    {
+        id: 1,
+        email: "rk@gmail.com"
+    },
+    {
+        id: 1,
+        email: "rk@gmail.com"
+    },
+]
+
+users[1].email
 console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
