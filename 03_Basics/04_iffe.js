82f7445c1ad46c7781a4d49e6e8f59ba04f99dc3
RavigiriG <104981318+RavigiriG@users.noreply.github.com>
2023-12-11 19:16:01 +0000

    IFFE

---
+++ 03_Basics/04_iffe.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    console.log(`DB CONECTED`);
+})();                           // Give semicolan to end the iffe.
+
+( () => {
+    console.log(`DB CONNECTED 2`);
+})();
